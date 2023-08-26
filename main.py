b1a56294ab11f8c68fe2ccc5f430e1f09d624cee
Dragan Stancevic <draganstancevic15@gmail.com>
2023-08-26 16:01:01 +0200

    Added clarifai

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from clarifai import Generate
 import streamlit as st
 
 
 st.write("What is going on??")
+
+
+results = Generate()
+
+out = results.outputs
+outResult = out[-1]
+
+st.write(outResult.data.text.raw)

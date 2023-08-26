f4d0c48c004b73c02de1ce9a3b4d14a4256eb43b
AleksaBajat <aleksabajat2001@gmail.com>
2023-08-26 18:29:48 +0200

    Small refactor

--- clarifai.py
+++ clarifai.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from clarifai_grpc.grpc.api.status import status_code_pb2
+
 
 @st.cache_data
 def get_credentials():
@@ -6,37 +10,26 @@ def get_credentials():
     # In this section, we set the user authentication, app ID, workflow ID, and
     # image URL. Change these strings to run your own example.
     ##############################################################################
-
     user_id = 'sdragan15'
-    # Your PAT (Personal Access Token)
     pat = '73028d3a4be24e18a7fdad1320333fb0'
     app_id = 'cool_app'
     workflow_id = 'workflow-306cec'
-    image_url = 'https://samples.clarifai.com/metro-north.jpg'
-    return user_id, pat, app_id, workflow_id, image_url
+    return user_id, pat, app_id, workflow_id
 
 
-@st.cache_data
-def get_data_from_clarify(text: str, image: bytes) -> str:
-
-    ##########################################################################
-    # YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-    ##########################################################################
-
-    from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
-    from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
-    from clarifai_grpc.grpc.api.status import status_code_pb2
-
+def retrieve_clarifai_stub():
     channel = ClarifaiChannel.get_grpc_channel()
     stub = service_pb2_grpc.V2Stub(channel)
+    return stub
 
-    user_id, pat, app_id, workflow_id, image_url = get_credentials()
 
-    metadata = (('authorization', 'Key ' + pat),)
+def clarify_image_to_story(text: str, image: bytes) -> str:
+    user_id, pat, app_id, workflow_id = get_credentials()
 
     user_data_object = resources_pb2.UserAppIDSet(user_id=user_id,
-                                                app_id=app_id)  # The userDataObject is required when using a PAT
+                                                  app_id=app_id)
 
+    stub = retrieve_clarifai_stub()
     post_workflow_results_response = stub.PostWorkflowResults(
         service_pb2.PostWorkflowResultsRequest(
             user_app_id=user_data_object,
@@ -51,26 +44,20 @@ def get_data_from_clarify(text: str, image: bytes) -> str:
                 )
             ]
         ),
-        metadata=metadata
+        metadata=(('authorization', 'Key ' + pat),)
     )
+
     if post_workflow_results_response.status.code != status_code_pb2.SUCCESS:
         print(post_workflow_results_response.status)
         raise Exception("Post workflow results failed, status: " + post_workflow_results_response.status.description)
 
-    # We'll get one WorkflowResult for each input we used above. Because of one input, we have here one WorkflowResult
     results = post_workflow_results_response.results[0]
-
-    # Each model we have in the workflow will produce one output.
-    for output in results.outputs:
-        model = output.model
-
-        for concept in output.data.concepts:
-            print("	%s %.2f" % (concept.name, concept.value))
-
-    # Uncomment this line to print the full Response JSON
-    # print(results.status)
-
     outputs = results.outputs
     result = outputs[-1]
-
     return result.data.text.raw
+
+
+@st.cache_data
+def get_data_from_clarify(text: str, image: bytes) -> str:
+    image_story = clarify_image_to_story(text, image)
+    return image_story
